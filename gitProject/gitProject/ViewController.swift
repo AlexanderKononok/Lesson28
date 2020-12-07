ef064d4dd60dc7a58c291d687f1db498fb5ff3d4
Alexander <alex.kononok@gmail.com>
2020-12-07 20:13:17 +0300

    Initial commit

---
+++ gitProject/gitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitProject
+//
+//  Created by Alexander Kononok on 12/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
